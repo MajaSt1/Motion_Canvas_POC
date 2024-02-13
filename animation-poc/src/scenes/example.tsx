1218ba6df0ea4f59f4a7fd8290fd34aafc0fd966
fms <fms@kmdpoland.pl>
2024-02-13 21:27:28 +0100

    simple rects movement

---
+++ animation-poc/src/scenes/example.tsx
@@ -0,0 +1,29 @@
+import {makeScene2D, Rect} from '@motion-canvas/2d';
+import {all, makeRef, range, waitFor} from '@motion-canvas/core';
+
+// tweening effect for cat
+// Path (Components) for cat tail
+export default makeScene2D(function* (view) {
+    const rects: Rect[] = [];
+
+    // Create some rects
+    view.add(
+        range(5).map(i => (
+            <Rect
+                ref={makeRef(rects, i)}
+                width={100}
+                height={100}
+                x={-250 + 125 * i}
+                fill="#88C0D0"
+                radius={10}
+            />
+        )),
+    );
+
+    yield* waitFor(1);
+
+    // Animate them
+    yield* all(
+        ...rects.map(rect => rect.position.y(100, 1).to(-100, 2).to(0, 1)),
+    );
+});
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+/**
+ * User: ${USER}
+ * Date: ${DATE}
+ */
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+  }
+}
